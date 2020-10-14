271b2621d845028217c68eafa710158caeebb640
Zorry <zorian.ivanec@gmail.com>
2020-10-14 23:11:18 +0300

    * updated var name

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -13,13 +13,13 @@ public class FirstPersonController : MonoBehaviour
 	public bool canJump = true;
 	public float jumpHeight = 2.0f;
 	private bool grounded = false;
-	private Rigidbody rigidbody;
+	private Rigidbody thisRigidbody;
 
 	void Awake()
 	{
-		rigidbody = GetComponent<Rigidbody>();
-		rigidbody.freezeRotation = true;
-		rigidbody.useGravity = false;
+		thisRigidbody = GetComponent<Rigidbody>();
+		thisRigidbody.freezeRotation = true;
+		thisRigidbody.useGravity = false;
 	}
 
 	private void Update()
@@ -37,23 +37,23 @@ public class FirstPersonController : MonoBehaviour
 			targetVelocity *= speed;
 
 			// Apply a force that attempts to reach our target velocity
-			Vector3 velocity = rigidbody.velocity;
+			Vector3 velocity = thisRigidbody.velocity;
 			Vector3 velocityChange = (targetVelocity - velocity);
 			velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
 			velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
 			velocityChange.y = 0;
-			rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+			thisRigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 
 			// Jump
 			if (canJump && Input.GetButton("Jump"))
 			{
-				rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+				thisRigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
 			}
 		}
 		Look();
 
 		// We apply gravity manually for more tuning control
-		rigidbody.AddForce(new Vector3(0, -gravity * rigidbody.mass, 0));
+		thisRigidbody.AddForce(new Vector3(0, -gravity * thisRigidbody.mass, 0));
 
 		grounded = false;
 	}
